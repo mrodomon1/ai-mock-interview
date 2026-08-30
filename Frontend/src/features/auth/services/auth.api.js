cc3b39f57d668250266d5996a12e3f52719555e3
mrodomon <worksrahul8@gmail.com>
2026-08-30 07:25:34 +0530

    feat: use vercel rewrites to fix incognito third-party cookie blocking

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,6 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "https://ai-mock-interview-1a6e.onrender.com",
     withCredentials: true
 })
 
--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "https://ai-mock-interview-1a6e.onrender.com",
     withCredentials: true,
 })
 
