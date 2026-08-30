467f4b33452ad5d851cab84c7a97dade515a1c0a
mrodomon <worksrahul8@gmail.com>
2026-08-30 06:46:32 +0530

    fix: correct render URL typo

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "https://ai-mock-interview-la6e.onrender.com",
+    baseURL: import.meta.env.VITE_API_URL || "https://ai-mock-interview-1a6e.onrender.com",
     withCredentials: true,
 })
 
